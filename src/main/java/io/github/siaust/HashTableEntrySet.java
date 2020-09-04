5073127b9d04e5d28f71dae651f030371ec6f189
Simon Aust <simon.aust@googlemail.com>
2020-09-04 10:22:24 +0100

    first commit

---
+++ src/main/java/io/github/siaust/HashTableEntrySet.java
@@ -0,0 +1,11 @@
+package io.github.siaust;
+
+public class HashTableEntrySet {
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        HashTableEntrySet hashTableEntrySet = new HashTableEntrySet();
+    }
+}
